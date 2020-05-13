6209f3e106c8b08e51c06b63a90d2dc797d28c85
Scott Andrews <sandrews@pivotal.io>
2020-05-13 11:05:35 -0400

    Drop GCS ACL since the bucket has a uniform access policy now

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -12,11 +12,11 @@ readonly git_branch=${1:11} # drop 'refs/head/' prefix
 
 echo "Publishing riff http-gateway"
 
-gsutil cp -a public-read gs://projectriff/riff-http-gateway/snapshots/riff-http-gateway-${slug}.yaml gs://projectriff/riff-http-gateway/riff-http-gateway-${version}.yaml
+gsutil cp gs://projectriff/riff-http-gateway/snapshots/riff-http-gateway-${slug}.yaml gs://projectriff/riff-http-gateway/riff-http-gateway-${version}.yaml
 
 echo "Publishing version references"
-gsutil -h 'Content-Type: text/plain' -h 'Cache-Control: private' cp -a public-read <(echo "${slug}") gs://projectriff/riff-http-gateway/snapshots/versions/${git_branch}
-gsutil -h 'Content-Type: text/plain' -h 'Cache-Control: private' cp -a public-read <(echo "${slug}") gs://projectriff/riff-http-gateway/snapshots/versions/${version}
+gsutil -h 'Content-Type: text/plain' -h 'Cache-Control: private' cp <(echo "${slug}") gs://projectriff/riff-http-gateway/snapshots/versions/${git_branch}
+gsutil -h 'Content-Type: text/plain' -h 'Cache-Control: private' cp <(echo "${slug}") gs://projectriff/riff-http-gateway/snapshots/versions/${version}
 if [[ ${version} != *"-snapshot" ]] ; then
-  gsutil -h 'Content-Type: text/plain' -h 'Cache-Control: private' cp -a public-read <(echo "${version}") gs://projectriff/riff-http-gateway/versions/releases/${git_branch}
+  gsutil -h 'Content-Type: text/plain' -h 'Cache-Control: private' cp <(echo "${version}") gs://projectriff/riff-http-gateway/versions/releases/${git_branch}
 fi
